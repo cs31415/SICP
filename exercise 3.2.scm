6bfa656e7d2098eb9139b6fbe7ef32f377464f1d
Chandra Sivaraman <cs31415@gmail.com>
2016-06-16 07:13:38 -0700

    exercise 3.2

--- exercise 3.2.scm
+++ exercise 3.2.scm
@@ -1,10 +1,21 @@
-(define (make-monitored f))
-  (lambda (x)))
+(define (make-monitored f)
+  (let ((counter 0))
+    (lambda (x)
+      (if (equal? x 'how-many-calls?)
+          counter
+          (begin
+            (set! counter (+ counter 1))
+            (f x))))))
+
 
 (define s (make-monitored sqrt))
 
 (s 100)
-10
-
+;10
 (s 'how-many-calls?)
-1
+;1
+
+(s 36)
+;6
+(s 'how-many-calls?)
+;2
