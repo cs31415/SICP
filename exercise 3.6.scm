c42f95c90a31eeaf3699735cda44045b11f53e33
Chandra Sivaraman <cs31415@gmail.com>
2016-10-20 08:08:15 -0700

    exercise 3.6

--- exercise 3.6.scm
+++ exercise 3.6.scm
@@ -1,4 +1,54 @@
 (define (rand mode)
-  (cond ((= mode 'generate) ())
-        ((= mode 'reset) ())
-        (else ())))
+  (cond ((eq? mode 'generate) (random))
+        ((eq? mode 'reset) (lambda (x) (random-seed x)))
+        (else 
+         (error "rand: Invalid argument."))))
+
+((rand 'reset) 10)
+
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+;0.11586349506387651
+;0.21594793789954184
+;0.027273325173382566
+;0.7611891798040247
+;0.41418324344561314
+;0.9934134249645269
+;0.9984134316141704
+
+((rand 'reset) 10)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+;0.11586349506387651
+;0.21594793789954184
+;0.027273325173382566
+;0.7611891798040247
+;0.41418324344561314
+;0.9934134249645269
+;0.9984134316141704
+
+((rand 'reset) 20)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+;0.5971941589881632
+;0.20959420702317616
+;0.587684919414684
+;0.6188529426514665
+;0.469868790528902
+;0.44043617802921803
+;0.8670100582153751
