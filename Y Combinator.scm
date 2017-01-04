f5e3096379f6f7ca3a175eb07e96837caad65351
Chandra Sivaraman <cs31415@gmail.com>
2017-01-03 21:59:45 -0800

    Y combinator

--- Y Combinator.scm
+++ Y Combinator.scm
@@ -1,6 +1,7 @@
 ; Y Combinator - aka how to define a recursive function when the language doesn't
 ; natively support recursion
-; 
+; Courtesy Brian Harvey:
+; (https://www.youtube.com/watch?v=tYVbCmqFVhY&index=12&list=PLE741AD926AA152C4)
 ((lambda (n)
    ((lambda (f) (f f n))
     (lambda (fact n)
